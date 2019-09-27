4f4afad25db1f4de22d9797006f6cf932035decc
Lu-Huan <180356757@qq.com>
2019-09-27 22:52:10 +0800

    test

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main(int argv, char** argc) {
 		return 0;
 	}
 	std::string t0 = "address.exe ";
-	std::string t1 = argc[0];
+	std::string t1 = argc[1];
 	//std::string t1 = "in.txt ";
 	t1 += " ";
-	std::string t2 = argc[1];
+	std::string t2 = argc[2];
 	//std::string t2 = "out.json";
 	system((t0 + t1 + t2).data());
 	return 0;
