69d52ae16663e54fb5f1b7445dbdca2f6b5d3037
Lu-Huan <180356757@qq.com>
2019-10-03 22:24:52 +0800

    65

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
-#include<stdlib.h>
+#include<cstdlib>
 #include<string>
 int main(int argv, char** argc) {
 	if (argv<2)
 	{
 		return 0;
 	}
+	
 	std::string t0 = "address.exe ";
 	std::string t1 = argc[1];
-	//std::string t1 = "in.txt ";
 	t1 += " ";
 	std::string t2 = argc[2];
-	//std::string t2 = "out.json";
 	system((t0 + t1 + t2).data());
 	return 0;
 }
